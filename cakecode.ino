cdd0ca8ef263b05ab85a3302922b92ed85ad641d
maham000 <85379239+maham000@users.noreply.github.com>
2024-09-17 21:27:45 -0600

    Update cakecode.ino

--- cakecode.ino
+++ cakecode.ino
@@ -2,12 +2,11 @@
 // maham fatima
 // sept 18 2024
 // A0 - CPSC 599 Physical and Tangible HCI
-// i referenced the birthday tune playing from : https://forum.arduino.cc/t/playing-happy-birthday-to-you-on-arduino/303601
+// i referenced the birthday music playing from : https://forum.arduino.cc/t/playing-happy-birthday-to-you-on-arduino/303601
 // and learned how to use sound sensor and code from: https://kandrproject.blogspot.com/2018/01/arduino-with-sound-sensor-and-led-code.html
-
 // ----------------------------
 
-//https://forum.arduino.cc/t/playing-happy-birthday-to-you-on-arduino/303601 -> bday tune from here
+//https://forum.arduino.cc/t/playing-happy-birthday-to-you-on-arduino/303601 -> bday tune code below from here
 int pin[]= {523,523,587,523,698,659,0,   // Notes' array
            523,523,587,523,783,698,0,
            523,523,1046,880,689,658.25,587,
@@ -16,15 +15,16 @@ int time[]={250,250,500,500,500,500,500, // time array for each note
            250,250,500,500,500,500,500,
            250,250,500,500,500,500,1000, 
            250,250,500,500,500,500,500};
-           // modified timings for more upbeat version
+// I modified timings for more upbeat version from this code making it double the speed to speed it up
 
 //set pins for mic, led and speaker
 const int micpin=7;
 const int ledpin=13; 
 const int soundpin=A2;
+    
+// to check if user has blown into the mic (sound sensor) 
+const int threshold=200; // threshold for the microphone -> Learned about sound sensor and used code: https://kandrproject.blogspot.com/2018/01/arduino-with-sound-sensor-and-led-code.html
 
-// to check if user has blown into the mic (sound sensor)
-const int threshold=200; // threshold for the microphone
 
 void setup() {
 Serial.begin(9600); // set up serial comunication 
@@ -34,17 +34,16 @@ pinMode(micpin, OUTPUT); // set speaker to be output
 }
 
 void loop() {
-    // Learned about sound sensor and code from: https://kandrproject.blogspot.com/2018/01/arduino-with-sound-sensor-and-led-code.html
-
+    // Learned about sound sensor and used if statement code below from: https://kandrproject.blogspot.com/2018/01/arduino-with-sound-sensor-and-led-code.html
     // in this loop we check to see if someone has exceeded the threshold and if so the light turns of and music turns on 
     // else the light stays on
     int soundsensor=analogRead(soundpin); // reads analog data from sound sensor
 
     if (soundsensor>=threshold) {         // check if sound louder then the threshold 
     digitalWrite(ledpin,LOW);           // turns led on when sound registered at higher then threshold
-
+    // loop through and play bday song -> bday music code here
     for (int i= 0; i<28; i++){       // loop for the duration of one round of "happy birthday" song
-      tone(7, pin[i], time[i]);       // play which note and how long
+      tone(7, pin[i], time[i]);       // play which note and how long 
       delay (time[i]);                 // add a delay between notes       
     }
     delay(1000);                        // delay so that there is time to see it "blown out" before it starts again
